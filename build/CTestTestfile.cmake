5e552993f60871c0dba28362d5e4ca6813e8d2be
audreybender <32439874+audreybender@users.noreply.github.com>
2017-11-05 17:21:19 +0100

    Add files via upload
    Fill build folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/localguest/Documents/Neurone
+# Build directory: /home/localguest/Documents/Neurone/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testNeurone "testNeurone")
+subdirs(gtest)
