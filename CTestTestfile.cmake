f4efa2c6290cc58ed3e887aba881bfc1cf9b3cd0
audreybender <32439874+audreybender@users.noreply.github.com>
2017-11-12 02:35:19 +0100

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/localguest/Documents/cppcourse-Brunel/src
-# Build directory: /home/localguest/Documents/cppcourse-Brunel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testNeurone "testNeurone")
-subdirs(gtest)
